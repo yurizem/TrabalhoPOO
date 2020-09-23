e84cb7b12894b0a37c505f774419a6842c3210e5
YuriBueno <yurizem333@gmail.com>
2020-09-23 20:29:02 -0300

    pequena atualização

--- Trab.js
+++ Trab.js
@@ -3,11 +3,11 @@ const fs = require('fs')
 console.log(1)
 
 function callback(err, conteudo) {
+    console.log(2)
     console.log(err, String(conteudo))
 }
 
 fs.readFile('./text.txt', callback)
 
-console.log(2)
-
 console.log(3)
+
